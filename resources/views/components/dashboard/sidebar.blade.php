46962025a5dbb8e8a30f6d01645c76c2cf14146b
honorezemagho <honorezemagho@gmail.com>
2021-04-03 18:07:19 +0100

    update sidebar

--- resources/views/components/dashboard/sidebar.blade.php
+++ resources/views/components/dashboard/sidebar.blade.php
@@ -16,16 +16,26 @@
                 </svg>
 
                 <span class="mx-3">My Campaigns</span>
-            </a>
+        </a>
 
-        {{-- <a class="flex items-center mt-4 py-2 px-6 bg-gray-700 bg-opacity-25 text-gray-100" href="{{ route('categories.index') }}">
+        <a class="flex items-center mt-4 py-2 px-6 bg-gray-700 bg-opacity-25 text-gray-100" href="{{ route('my-donations') }}">
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
+                </svg>
+
+                <span class="mx-3">My Donations</span>
+        </a>
+
+ {{-- <a class="flex items-center mt-4 py-2 px-6 bg-gray-700 bg-opacity-25 text-gray-100" href="{{ route('categories.index') }}">
             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
             </svg>
 
-            <span class="mx-3">Categories</span>--}}
-        </a>
+            <span class="mx-3">Categories</span>
+        </a>--}}
 
     </nav>
 </div>
+
